347161ed4fc53c89ac5735087d3db81104f7e239
Patric Sok <v-pasok@microsoft.com>
2020-05-12 10:38:22 -0700

    change project name

--- ExsilioFramework.API/Data/DataContext.cs
+++ ExsilioFramework.API/Data/DataContext.cs
@@ -1,5 +1,6 @@
 using API.Models;
 using Microsoft.EntityFrameworkCore;
+using TeamProspect.API.Models;
 
 namespace API.Data
 {
@@ -8,5 +9,6 @@ namespace API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Value> Values {get; set;}
+        public DbSet<User> Users { get; set; }
     }
 }
